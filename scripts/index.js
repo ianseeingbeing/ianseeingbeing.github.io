e808361e0e632fc8e57f1f319c14f9e540f9a0ad
ianseeingbeing <igillette2006@gmail.com>
2024-06-13 23:06:58 -0400

    added simple hamberger menu

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,17 @@ const hambergerToggle = document.getElementById('hambergerToggle');
 const hambergerIconClosed = document.getElementById('hambergerIconClosed');
 const hambergerIconOpen = document.getElementById('hambergerIconOpen');
 
+const hambergerMenus = document.getElementsByClassName('hambergerMenu');
+
 hambergerToggle.onchange = function() {
     if (hambergerToggle.checked) {
-        openMenu();
+        hambergerMenus[0].hidden = false;
         hambergerIconClosed.hidden = true;
         hambergerIconOpen.hidden = false;
     }
     else {
-        closeMenu();
+        hambergerMenus[0].hidden = true;
         hambergerIconClosed.hidden = false;
         hambergerIconOpen.hidden = true;
     }
-}
-
-function openMenu() {
-    console.log('open') 
-}
-
-function closeMenu() {
-    console.log('closed')
 }
